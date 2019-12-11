cd3099cfabc724dbdfe9561756ed5efb0d6d438f
MS-2 <juanjosejjjj@gmail.com>
2019-12-11 09:25:54 -0800

    Updates

---
+++ precache-manifest.94d59925ed7bb660753c1e4ae27ab858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d17db36650ee59acf42771ab347457",
+    "url": "/appix/index.html"
+  },
+  {
+    "revision": "3fe56146ebfe8ae218fc",
+    "url": "/appix/static/css/main.634c48ca.chunk.css"
+  },
+  {
+    "revision": "c7244decf53618964fa4",
+    "url": "/appix/static/js/2.b490c8e6.chunk.js"
+  },
+  {
+    "revision": "9fc635aceec4ff3bb823043ab022e718",
+    "url": "/appix/static/js/2.b490c8e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fe56146ebfe8ae218fc",
+    "url": "/appix/static/js/main.77d07f67.chunk.js"
+  },
+  {
+    "revision": "f07737bb1ab28c85463d",
+    "url": "/appix/static/js/runtime-main.1b96f616.js"
+  }
+]);
